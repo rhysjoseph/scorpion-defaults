d33e9e8c810a214231fc0944908846f4a31deb22
User <Email>
2024-04-26 17:00:44 +0000

    readme and cleanup

--- docs/README.md
+++ docs/README.md
@@ -17,26 +17,18 @@ scripts/install.sh
 
 _Web GUI address x.x.x.x/settings_
 
--   Network
-
-    -   Set Static IP - Changes mode to static and sets address
-
-        > <img src="assets/network.png" alt="network" width="500"/>
-
-    -   Bitmask Calculator - computes subnet mask address from bit mask
-
-        > <img src="assets/bitmask.png" alt="bitmask" width="500"/>
+-   Scorpions
+> Set Defaults - sets all default parameters in default_parameters in json file.
+> Displays current params and allows setting default individually 
 
 ---
 
 ### CLI
 
+> Future Use
+
 | Title             | Command                                    | Desscription                                        |
 | ----------------- | ------------------------------------------ | --------------------------------------------------- |
 | Version           | `flows --version`                          | Returns current cli version                         |
 | Update            | `flows update`                             | Software Update                                     |
-| Net - DHCP        | `flows net dhcp`                           | Switches device to DHCP mode                        |
-| Net - Static      | `flows net static *ip address* *gateway*`  | Switches device to static and sets address          |
-| Net - Reset       | `flows net reset`                          | Resets to static ip and sets mode to DHCP           |
-| Display - Stats   | `flows display stats --enable/--no-enable` | Runs system stats on device screen                  |
-| Display - Message | `flows display message *text*`             | Stops Stats and displays a message on device screen |
+
--- src/scorpion/default.py
+++ src/scorpion/default.py
@@ -21,7 +21,7 @@ def _split_dict(dict_, max_keys):
 
 
 def set_factory_defaults(scorpion):
-    with open("src/scorpion/parameters.json", "r") as f:
+    with open("src/scorpion/parameters_reference.json", "r") as f:
         parameters = json.load(f)
     for key in parameters:
         control_indexes = parameters[key].get("control-indexes")
@@ -37,51 +37,9 @@ def set_factory_defaults(scorpion):
 
 
 def get_user_defaults():
-    return {
-        "59": 1,  # fpga to 2110
-        "5100": 127,  # network_ptp_domain
-        "116": 30,  # fan speed
-        "120": 1,  # time_source
-        "122": -7,  # time_offset
-        "123": 0,  # time_daylight
-        "124": "10.244.240.1",  # time_server
-        "164": 0,  # ref frequency and time ref source
-        "165": 1,  # sync ref source
-        "166": 0,  # sync standard
-        "5200": 1,  # nmos_support
-        "5201": 1,  # nmos_dns
-        "5202": "testsuite.nmos.tv",  # nmos_domain
-        "5203": "172.16.126.121",  # nmos_name_server
-        "5204": "get_name",  # nmos_device_name
-        "5206": 0,  # nmos_endpoint
-        "6000.0": "10.101.245.",  # trunk_ip
-        "6001.0": "255.255.0.0",  # trunk_net_mask
-        "6002.0": "10.101.0.1",  # trunk_gateway
-        "6018.0": 2,  # data rate
-        "6022.0": 0,  # dhcp off
-        "6000.1": "10.102.245.",  # trunk_ip
-        "6001.1": "255.255.0.0",  # trunk_net_mask
-        "6002.1": "10.102.0.1",  # trunk_gateway
-        "6018.1": 2,  # data rate
-        "6022.1": 0,  # dhcp off
-        "6213.0": 1,  # input video redundancy mode non-invertive
-        "6213.1": 1,
-        "6213.2": 1,
-        "6213.3": 1,
-        "6213.4": 1,
-        "6213.5": 1,
-        "6213.6": 1,
-        "6213.7": 1,
-        "6659.0": 1,  # output audio mapping pair
-        "6659.1": 1,
-        "6659.2": 1,
-        "6659.3": 1,
-        "6659.4": 1,
-        "6659.5": 1,
-        "6659.6": 1,
-        "6659.7": 1,
-    }
-
+    with open("src/scorpion/default_parameters.json", encoding="utf-8") as f:
+        defaults = json.load(f)
+    return defaults
 
 def get_nmos_name(name):
     name = name.upper()
