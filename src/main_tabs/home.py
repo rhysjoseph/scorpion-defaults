cd1b12fcab969d347d9cad3dad54b8e69ce08e62
User <Email>
2025-01-17 17:09:28 +0000

    cleanup

--- src/main_tabs/home.py
+++ src/main_tabs/home.py
@@ -7,7 +7,7 @@ def tab(config, scorpions, mcms, switches):
     col1.link_button("hi",f"http://{config['LINKS']['hi']}", use_container_width=True)
     col2.link_button("Prism",f"http://{config['LINKS']['Prism']}", use_container_width=True)
 
-    col1, col2,col3, col4 = st.columns([1,.7,.24,1])
+    col1, col2,col3, col4 = st.columns([1,.5,.5,1])
     selected_cisco = col1.selectbox("Select Switch:", config["SWITCH_LIST"])
     col2.write("")
     col2.write("")
@@ -15,12 +15,12 @@ def tab(config, scorpions, mcms, switches):
 
     col3.write("")
     col3.write("")
-    if col3.button("Ping"):
+    if col3.button("Ping", use_container_width=True):
         if utils.ping(config['SWITCH_LIST'][selected_cisco], timeout=2):
             st.info("PONG")
         else:
             st.error("WA WA")
-
+    col4.write("")
     col4.write("")
     ssh_command = f"ssh admin@{config['SWITCH_LIST'][selected_cisco]}"
     col4.code(ssh_command, language="python")        
