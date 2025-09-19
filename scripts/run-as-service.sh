a398ed27bdaf4654c5e82c3fec6659ba53c4d6af
Rhys Joseph <rhys.v.joseph@gmail.com>
2025-09-19 15:02:37 +0100

    updated port details to 8501

--- scripts/run-as-service.sh
+++ scripts/run-as-service.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+!/bin/bash
 
-# sudo mkdir -p /etc/docker/compose/scorpion-defaults
-# sudo cp docker-compose.yaml /etc/docker/compose/scorpion-defaults/docker-compose.yaml
-# sudo cp .env /etc/docker/compose/scorpion-defaults/.env
+ sudo mkdir -p /etc/docker/compose/scorpion-defaults
+ sudo cp docker-compose.yaml /etc/docker/compose/scorpion-defaults/docker-compose.yaml
+ sudo cp .env /etc/docker/compose/scorpion-defaults/.env
 
-sudo cp scripts/docker-compose.service /etc/systemd/system/docker-compose@.service
+# sudo cp scripts/docker-compose.service /etc/systemd/system/docker-compose@.service
 
-sudo systemctl daemon-reload
-sudo systemctl enable docker-compose@scorpion-defaults
-sudo systemctl start docker-compose@scorpion-defaults
+# systemctl daemon-reload
+# sudo systemctl enable docker-compose@scorpion-defaults
+# sudo systemctl start docker-compose@scorpion-defaults
